66ca5beb6da183d9b69d513445c5590ea0ff9b8f
Alejandro Bermúdez <alejucha@gmail.com>
2026-01-15 14:08:34 -0500

    Convertir en PWA

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public", // Donde pondrá los archivos de la app
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  disable: process.env.NODE_ENV === "development", // Desactivar en localhost para que no moleste
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Tu configuración actual si tienes alguna, si no, déjalo vacío
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
